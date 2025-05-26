a87bc407cc76b48754020e75af208ba52c547b35
enzolebasrabelais <enzolebas190106@gmail.com>
2025-05-26 12:16:04 +0200

    debut uc5 4

--- app/Views/Visiteur/vue_LiaisonsParSecteur.php
+++ app/Views/Visiteur/vue_LiaisonsParSecteur.php
@@ -1,13 +1,32 @@
 <?php
+echo "<table class='table table-striped'>";
+echo "
+<tr>
+<th>Secteur</th>
+<th>Code liaison</th>
+<th>Distance</th>
+<th>Port de départ</th>
+<th>Port d'arrivée</th>
+</tr>";
+foreach ($liaisons as $uneLiaison)
+{
+    echo "<TR>";
+    echo /*"<TD>".$uneLiaison->nomdusecteur."</TD>*/"<TD>"
+    .$uneLiaison->NoLiaison."</TD><TD>"
+    .$uneLiaison->distance."</TD><TD>"
+    .$uneLiaison->portd."</TD><TD>"
+    .$uneLiaison->porta."</TD>";
+    echo "</TR>";
+}
+echo "</table>";
 
-$attributsTableau = ["table_open" => "<table class='table table-striped'>",]; // classe Bootstrap
+
+/*foreach ($liaisons as $uneLiaison)$attributsTableau = ["table_open" => "<table class='table table-striped'>",]; // classe Bootstrap
 $table = new \CodeIgniter\View\Table($attributsTableau);
 $table->setHeading(['Secteur', 'Code Liaison', 'Distance', 'Port de départ',
 'Port arrivée']); // entête tableau
 
 echo $table->generate($liaisons);
-
-/*foreach ($liaisons as $uneLiaison)
 {?>
     <a href="<?php echo site_url('')?>"><?php $uneLiaison->noliaison;?></a><?php
 }*/
